cde08e1a6d80513e93410d481e2318da4883a65e
Leticia Muller <leticia.muller@sap.com>
2019-06-05 21:27:50 -0300

    created project

---
+++ src/test/java/com/sort/sortingalgorithms/SortingAlgorithmsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sort.sortingalgorithms;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SortingAlgorithmsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
